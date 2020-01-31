44cbfcf59b5fa0a57faba76629cf168b6a2805d8
Ethan Davidson <theejdavidson@gmail.com>
2020-01-31 22:38:23 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,11 +11,11 @@ end
 
 def reduce(source_array, starting_point = nil)
   if starting_point
-    num1 = starting_point
-    sum = 0 
+    sum = starting_point
+    i = 0 
   else
-    num1 = source_array[0]
-    sum = 1
+    sum = source_array[0]
+    i = 1
   end
     
   while i < source_array.length do
