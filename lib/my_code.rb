8bda0972da8e51207f0e9a6a7c4952a9bf28b858
Ethan Davidson <theejdavidson@gmail.com>
2020-01-31 22:32:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,7 +10,13 @@ def map(source_array)
 end
 
 def reduce(source_array, starting_point = nil)
-  if starting_point != nil
+  if starting_point
+    num1 = starting_point
+    i = 0 
+  else
+    num1 = source_array[0]
+    i = 1
+  end
     
   index = 0 
   while index < source_array.length do
