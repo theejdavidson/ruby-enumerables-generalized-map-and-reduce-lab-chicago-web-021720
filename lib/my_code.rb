66ba3ae6882dd2821dc6c5eed67dee1dcf53df46
Ethan Davidson <theejdavidson@gmail.com>
2020-01-31 22:34:15 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,18 +13,12 @@ def reduce(source_array, starting_point = nil)
   if starting_point
     num1 = starting_point
     i = 0 
-    while index < source_array.length do
-      i += 1
-    end
+  else
     num1 = source_array[0]
     i = 1
-    while index < source_array.length do
-      i += 1
-    end
   end
     
-  index = 0 
-  while index < source_array.length do
+  while i < source_array.length do
     result = yield(source_array[index])
     if(source_array[index])
       starting_point += yield(source_array[index])
