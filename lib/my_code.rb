ad65fec52b2178a909ed719bb9317a7dc625994f
Ethan Davidson <theejdavidson@gmail.com>
2020-01-31 22:09:16 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,20 @@
 # Your Code Here
+def map(source_array)
+  mapped = []
+  index = 0 
+  while index < source_array.length do
+    mapped << yield(source_array[index])
+    index += 1 
+  end
+  return mapped
+end
+
+def reduce(source_array, starting_point = 0)
+  index = 0 
+  while index < source_array.length do
+      starting_point += yield(source_array[index])
+    
+    index += 1 
+  end
+  return starting_point
+end
